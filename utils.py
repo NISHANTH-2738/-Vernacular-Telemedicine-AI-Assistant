04de12c1a2e9e845d7dba1312b2556bde471f935
NISHANTH-2738 <134719640+NISHANTH-2738@users.noreply.github.com>
2025-07-11 08:14:41 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 from gtts import gTTS
 from deep_translator import GoogleTranslator
+import os
 
+# Translation using Deep Translator
 def translate_text(text, src="ta", dest="en"):
     return GoogleTranslator(source=src, target=dest).translate(text)
 
-
+# Convert text to speech and play it
 def speak_text(text, lang="ta"):
     tts = gTTS(text=text, lang=lang)
     tts.save("reply.mp3")
-    os.system("start reply.mp3")
+    os.system("start reply.mp3")  # On Windows
