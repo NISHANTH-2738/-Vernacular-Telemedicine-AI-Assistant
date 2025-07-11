51d3fa7e60148c051c6352613858be39cfb98a6b
NISHANTH-2738 <134719640+NISHANTH-2738@users.noreply.github.com>
2025-07-11 08:09:57 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,9 @@
 from gtts import gTTS
-from googletrans import Translator
-import os
-
-translator = Translator()
+from deep_translator import GoogleTranslator
 
 def translate_text(text, src="ta", dest="en"):
-    result = translator.translate(text, src=src, dest=dest)
-    return result.text
+    return GoogleTranslator(source=src, target=dest).translate(text)
+
 
 def speak_text(text, lang="ta"):
     tts = gTTS(text=text, lang=lang)
